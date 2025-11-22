40aee6aa1f729c80f89e17ff841acd32850ba524
Roman Mesler <meslerdev@gmail.com>
2025-11-22 14:26:25 +0500

    init

---
+++ src/main/java/ru/urfu/web/store/authorization/StoreAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.web.store.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAuthServiceApplication.class, args);
+	}
+
+}
